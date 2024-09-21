46fe4e083ccbddac3be55a0c3da385dd4706ecba
igorcfig <igorcfig@gmail.com>
2024-09-21 11:44:14 -0300

    Distribution done

--- Line Theory ( Jun Party)/main.py
+++ Line Theory ( Jun Party)/main.py
@@ -1,5 +1,5 @@
 import numpy as  np
-import pandas as pd
+# import pandas as pd
 import matplotlib.pyplot as plt
 
 
@@ -20,19 +20,61 @@ def dist_normal(mu, std, t):
     return norm
 
 def line(arrive, server):
+    # print(1)
     
     wait_time = []
     len_line = []
+    len_line.append(arrive[0])
 
-    for i in arrive:
-        
+    i=1
+    validation = False
+
+    # print(i)
+    while validation == False:
+        # print()
+        if i <= (len(arrive)-1):
+            if len_line[i-1] <= 0:
+                add =  arrive[i]
+                len_line.append(add)
+            else:
+                add = len_line[i-1]+ arrive[i] - server
+                len_line.append(add)
+        else:
+            if len_line[i-1] <= 0:
+                validation = True
+            else :
+                add = len_line[i-1] - server
+                len_line.append(add)
+
+            # print('3')
+            
+         
+        i = i+1
+
+    return len_line 
 
 
 distr = dist_normal(mu = minutes/2 , std = 30 ,t = time)
 
+
 distr = [ int(i * total) for i in distr]
-print(distr)
+scale  = total/sum(distr)
+# print(sum(distr))
+distr = [ int(i * scale) for i in distr]
 
+
+
+
+print(sum(distr))
+
+df = line(distr,5)
+print(len(df))
+
+print(df)
+
+
+plt.plot(list(range(0, len(df))),df)
+plt.show()
     
-
+print(len(df))
 
